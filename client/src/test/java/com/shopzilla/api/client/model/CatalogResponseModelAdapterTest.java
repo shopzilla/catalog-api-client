42e0d851c261e50533518f84d0e3cac45b4656e5
Andrew Look <alook@shopzilla.com>
2011-07-20 20:38:47 -0700

    Fixes to client

--- client/src/test/java/com/shopzilla/api/client/model/CatalogResponseModelAdapterTest.java
+++ client/src/test/java/com/shopzilla/api/client/model/CatalogResponseModelAdapterTest.java
@@ -19,7 +19,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
-import com.shopzilla.services.catalog.*;
 import org.junit.Before;
 import org.junit.Test;
 
