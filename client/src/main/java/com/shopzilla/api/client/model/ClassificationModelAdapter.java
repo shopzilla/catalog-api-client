9e1fed3161f8cd6fcfc87c1bd9a52057477cb6dd
Tom Suthanurak <tsuthanurak@shopzilla.com>
2012-01-03 21:40:57 -0800

    set suggestion result in classification model.

--- client/src/main/java/com/shopzilla/api/client/model/ClassificationModelAdapter.java
+++ client/src/main/java/com/shopzilla/api/client/model/ClassificationModelAdapter.java
@@ -7,19 +7,24 @@ package com.shopzilla.api.client.model;
 import java.util.List;
 
 import org.apache.commons.lang.BooleanUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import com.shopzilla.api.client.model.response.Classification;
 import com.shopzilla.services.catalog.ClassificationResponse;
 import com.shopzilla.services.catalog.QuerySuggestionType;
 
 /**
- * Description of what the class is about
+ * Convert service classification response into API response model.
  * 
  * @author Tom Suthanurak
  * 
  * @since Jan 3, 2012
  */
 public class ClassificationModelAdapter {
+    
+    private static final Log LOG = LogFactory.getLog(ClassificationModelAdapter.class);
+    
     public static Classification fromCatalogAPI(ClassificationResponse response) {
         final Classification output = new Classification();
         
@@ -38,18 +43,32 @@ public class ClassificationModelAdapter {
     
     private static void setSuggestions(Classification classification, List<QuerySuggestionType> querySuggestions) {
         if (querySuggestions != null) {
+            
             for (QuerySuggestionType querySuggestion : querySuggestions) {
+                
                 Suggestion suggestion = new Suggestion();
-                suggestion.setSuggestionType(Suggestion.SuggestionType.valueOf(querySuggestion.getId()));
+                try {
+                    suggestion.setSuggestionType(Suggestion.SuggestionType.valueOf(querySuggestion.getId()));
+                } catch (IllegalArgumentException e) {
+                    LOG.error("Unable to convert query suggestion type to a known enum, type:"+querySuggestion.getId(), e);
+                }
+                
                 suggestion.setKeyword(querySuggestion.getKeyword());
                 
                 if (querySuggestion.getOffer() != null) {
-                    CatalogResponseModelAdapter.convertOffer(querySuggestion.getOffer());
+                    suggestion.setOffer(CatalogResponseModelAdapter.convertOffer(querySuggestion.getOffer()));
                 }
                 if (querySuggestion.getProduct() != null) {
-                    CatalogResponseModelAdapter.convertProduct(querySuggestion.getProduct());
+                    suggestion.setProduct(CatalogResponseModelAdapter.convertProduct(querySuggestion.getProduct()));
                 }
-                
+                if (querySuggestion.getCategory() != null) {
+                    Category category = new Category();
+                    category.setId(querySuggestion.getCategory().getId());
+                    category.setName(querySuggestion.getCategory().getName());
+                    suggestion.setCategory(category);
+                }
+
+                classification.getSuggestions().add(suggestion);
             }
         }
     }
