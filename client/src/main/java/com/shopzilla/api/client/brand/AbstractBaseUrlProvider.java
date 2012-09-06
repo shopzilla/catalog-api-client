50ad53ed5cce518bdee39a85db06640edf17a709
John Lindwall <jlindwall@shopzilla.com>
2012-09-06 10:04:16 -0700

    S7764: Use prod catalog api servers for unit tests

--- client/src/main/java/com/shopzilla/api/client/brand/AbstractBaseUrlProvider.java
+++ client/src/main/java/com/shopzilla/api/client/brand/AbstractBaseUrlProvider.java
@@ -42,7 +42,7 @@ public abstract class AbstractBaseUrlProvider implements UrlProvider {
 
     private static final String EMPTY_STRING = "";
 
-    public String apiBaseUrl = "http://apistage001.sl1.shopzilla.seastg:7046/services/catalog/v1/api/";
+    public String apiBaseUrl = "http://apiprod006.sl1.shopzilla.sea:7046/services/catalog/v1/api/";
 
     public static final String PRODUCT_SERVICE_ENDPOINT_NAME = "product/";
     public static final String ATTRIBUTE_SERVICE_ENDPOINT_NAME = "attributes/";
